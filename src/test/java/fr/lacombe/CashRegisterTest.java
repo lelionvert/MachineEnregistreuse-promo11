43511eecb6d05a0931e24117744dfce33f2330b3
20Bak <back.to.bak@gmail.com>
2019-10-28 14:09:03 +0100

    Refactoring test using method source

--- src/test/java/fr/lacombe/CashRegisterTest.java
+++ src/test/java/fr/lacombe/CashRegisterTest.java
@@ -1,22 +1,34 @@
 package fr.lacombe;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 class CashRegisterTest {
+    static Stream<Arguments> priceProvider() {
+        return Stream.of(
+                arguments(new Price(1.2), 1, new Price(1.2)),
+                arguments(new Price(1.2), 2, new Price(2.4)),
+                arguments(new Price(3.5), 4, new Price(14))
+        );
+    }
+
     @ParameterizedTest
-    @CsvSource({"1.2, 1, 1.2", "1.2, 2, 2.4", "3.5, 4, 14"})
-    void cash_register_calculates_total_price(double price, int quantity, double result) {
+    @MethodSource("priceProvider")
+    void cash_register_calculates_total_price(Price price, int quantity, Price result) {
         // Given
         CashRegister cashRegister = new CashRegister();
 
         // When
-        Price total = cashRegister.total(new Price(price), quantity);
+        Price total = cashRegister.total(price, quantity);
 
         // Then
-        assertThat(total).isEqualTo(new Price(result));
+        assertThat(total).isEqualTo(result);
     }
 }
