1fc033df223d4f3a4700180cf641b0f6ed5e5060
20Bak <back.to.bak@gmail.com>
2019-10-28 11:32:53 +0100

    generalize total method in CashRegister

--- src/main/java/fr/lacombe/CashRegister.java
+++ src/main/java/fr/lacombe/CashRegister.java
@@ -2,9 +2,6 @@ package fr.lacombe;
 
 class CashRegister {
     double total(double price, double quantity) {
-        if (quantity == 2) {
-            return 2.4;
-        }
-        return 1.2;
+        return quantity * price;
     }
 }
