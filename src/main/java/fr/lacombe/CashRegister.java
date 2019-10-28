840f90c8009a6e957122079be92ffb75dc3794c0
bouchra94 <essaghir.bouchra@gmail.com>
2019-10-28 14:32:05 +0100

    Suppression total(double, double)

--- src/main/java/fr/lacombe/CashRegister.java
+++ src/main/java/fr/lacombe/CashRegister.java
@@ -1,10 +1,6 @@
 package fr.lacombe;
 
 class CashRegister {
-    double total(double price, double quantity) {
-        return price * quantity;
-    }
-
     Price total(Price price, double quantity) {
         return new Price(price.getPrice() * quantity);
     }
--- src/test/java/fr/lacombe/CashRegisterTest.java
+++ src/test/java/fr/lacombe/CashRegisterTest.java
@@ -9,26 +9,6 @@ import org.junit.runner.RunWith;
 @RunWith(JUnitParamsRunner.class)
 public class CashRegisterTest
 {
-
-    private Object[] parametersForCreate_cash_register() {
-        return new Object[][] {
-                {1.20, 0.0, 0.0},
-                {1.20, 1.0, 1.20},
-                {1.20, 2.0, 2.40},
-                {1.20, 5.0, 6.00}
-        };
-    }
-
-    @Test
-    @Parameters
-    public void create_cash_register(final double price, final double quantity, final double expected) {
-        CashRegister cash_register = new CashRegister();
-
-        double total = cash_register.total(price, quantity);
-
-        Assertions.assertThat(total).isEqualTo(expected);
-    }
-
     private Object[] parametersForCreate_cash_register_price_concept() {
         return new Object[][] {
                 {new Price(1.20), 0.0, new Price(0.0)},
