8f402da14bcc0d3c90ce9a1c6c055b1e3af894be
20Bak <back.to.bak@gmail.com>
2019-10-28 11:07:40 +0100

    Environment ready

---
+++ src/test/java/fr/lacombe/AppTest.java
@@ -0,0 +1,13 @@
+package fr.lacombe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class AppTest {
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
