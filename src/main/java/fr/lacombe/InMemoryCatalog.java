fb86ba590236bde92a38eee47ca5ade8a8675013
20Bak <back.to.bak@gmail.com>
2019-10-29 17:57:08 +0100

    Use stream in findPrice

--- src/main/java/fr/lacombe/InMemoryCatalog.java
+++ src/main/java/fr/lacombe/InMemoryCatalog.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 class InMemoryCatalog implements PriceQuery {
     private final List<ItemReference> itemReferences;
@@ -14,17 +15,12 @@ class InMemoryCatalog implements PriceQuery {
 
     @Override
     public Result findPrice(String itemCode) {
-        return reduce(Result.notFound(itemCode),
-                (Result result, ItemReference itemReference) ->
-                        itemReference.matchSoughtItem(itemCode) ? Result.found(itemReference.getPrice()) : result,
-                itemReferences);
+        return itemReferences.stream()
+                .filter(itemReference -> itemReference.matchSoughtItem(itemCode))
+                .findAny()
+                .map(ItemReference::getPrice)
+                .map(Result::found)
+                .orElse(Result.notFound(itemCode));
     }
 
-    private <T, R> T reduce(T defaultValue, BiFunction<T, R, T> reducer, Collection<R> collection) {
-        T accumulator = defaultValue;
-        for (R element : collection) {
-            accumulator = reducer.apply(accumulator, element);
-        }
-        return accumulator;
-    }
 }
